d0e7ceb77bc0f1837c938ce07ca75189d8bda3cf
FranzVisarra <FranzVisarra@gmail.com>
2023-11-29 01:32:00 -0800

    Fov Progress

--- Rogue Steel/Assets/PlFOV.cs
+++ Rogue Steel/Assets/PlFOV.cs
@@ -5,9 +5,58 @@ using UnityEngine;
 public class PlFOV : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        //thanks code monkey :)
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 2;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            float angleRad = angle * (Mathf.PI / 180f);
+            Vector3 vertex = origin + new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad)) * viewDistance;
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+                
+                triangleIndex += 3;
+            }
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        /*
+        vertices[0] = Vector3.zero;
+        vertices[1] = new Vector3(50,0);
+        vertices[2] = new Vector3(0, -50);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        */
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
     }
 
     // Update is called once per frame

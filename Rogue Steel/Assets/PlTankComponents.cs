8015bb37178ca34d05ef33bfcc35ea79a92aa05b
FranzVisarra <FranzVisarra@gmail.com>
2023-12-06 01:58:56 -0800

    Brb testing thingh

--- Rogue Steel/Assets/PlTankComponents.cs
+++ Rogue Steel/Assets/PlTankComponents.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlTankComponents : MonoBehaviour
 {
     public string[,] PlTank;
-    public string[,] Rcvd =new string[,]{{"cd","cg","am","en"},{"ar","cl","hd","fu"} };
+    public string[,] Rcvd =new string[2,4]{{"cd","cg","am","en"},{"ar","cl","hd","fu"} };
     // Start is called before the first frame update
     void Start()
     {

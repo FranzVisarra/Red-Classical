39abab67535ad65b32ad4c73092ed6a589d79648
FranzVisarra <FranzVisarra@gmail.com>
2023-12-06 09:36:50 -0800

    Redid Components
    Somehow Broke. Idk what happened

--- Rogue Steel/Assets/PlBodComponents.cs
+++ Rogue Steel/Assets/PlBodComponents.cs
@@ -7,21 +7,44 @@ public class PlBodComponents : MonoBehaviour
     // Start is called before the first frame update
     private int length = 6;//<->
     private int width = 3;
-    public GameObject cannonTemp;
-    public GameObject cannon;
+    //public GameObject cannonTemp;
+    //public GameObject cannon;
     public GameObject armorTemp;
     public GameObject armor;
+    private GameObject mechanics;
+    private GameObject componentTemp;
+    private GameObject component;
+    private GameObject Modules;
+    public string[,] innards;
     void Start()
     {
         //length = 3;
         //setup
-        //cannon
-        
-        //cannonTemp = GameObject.Find("Cannon");
+        /*--------------------      Cannon      --------------------//
+        cannonTemp = GameObject.Find("Cannon");
         cannon = Instantiate(cannonTemp, this.transform);
         cannon.transform.Translate(0, 0, 1);
         cannonTemp.SetActive(false);
-        //armor Tile
+        //--------------------      Cannon      --------------------*/
+        //--------------------  Tank Components --------------------//
+        mechanics = GameObject.Find("Mechanics");
+        PlTankComp innardsscript = mechanics.GetComponent<PlTankComp>();
+        innards = innardsscript.PlTank;
+        width = innards.GetLength(0);
+        length = innards.GetLength(1);
+        for (int i = 0; i < width; i++)
+        {
+            for (int n = 0; n < length; n++)
+            {
+                //get component
+                componentTemp = GameObject.Find(com(innards[i, n]));
+                component = Instantiate(componentTemp, this.transform);
+                //transform component
+                component.transform.Translate(0 - ((float)length / 2) + 0.5f + n, ((float)width / 2) - 0.5f - i, -1);
+            }
+        }
+        //--------------------  Tank Components --------------------//
+        //--------------------      Armor Tile  --------------------//
         //front
         directionalTiles(length,width,0);
         //back
@@ -31,12 +54,36 @@ public class PlBodComponents : MonoBehaviour
         //right
         directionalTiles(width,length,270);
         armorTemp.SetActive(false);
+        //--------------------      Armor Tile  --------------------//
+        Modules = GameObject.Find("Modules");
+        Modules.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public string com(string inp)
     {
-        
+        switch (inp)
+        {
+            case "am":
+                return "Ammunition";
+            case "ar":
+                return "Armor";
+            case "en":
+                return "Engine";
+            case "fu":
+                return "Fuel";
+            case "hd":
+                return "Horizontal Drive";
+            case "ra":
+                return "Radiator";
+            case "cd":
+                return "Driver";
+            case "cg":
+                return "Gunner";
+            case "cl":
+                return "Loader";
+            default:
+                return "Empty Cell";
+        }
     }
 
     public void directionalTiles(int forward, int side, int degrees)
